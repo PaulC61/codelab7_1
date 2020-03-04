5d939335ad117804d53a391b7d588b4231516913
Paul Clarke <58661391+PaulC61@users.noreply.github.com>
2020-03-04 17:45:54 +0100

    Async task tied to button is implemented.

--- ASyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
+++ ASyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    //I like cheese
+
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView1);
+    }
+
+    public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView).execute();
     }
 }
